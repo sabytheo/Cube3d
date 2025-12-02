adc21f2460df1f277809a8f99ae40b2bc6552bd3
Erwann Gache <egache@z4r5p4.42lyon.fr>
2025-12-02 15:01:02 +0100

    deleted somme error.h define and added some | deleted some maps

--- includes/error.h
+++ includes/error.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsaby <tsaby@student.42.fr>                +#+  +:+       +#+        */
+/*   By: egache <egache@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 13:18:37 by tsaby             #+#    #+#             */
-/*   Updated: 2025/11/28 19:02:00 by tsaby            ###   ########.fr       */
+/*   Updated: 2025/12/02 14:58:26 by egache           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 # define E_WRONG_INIT "Error :\nUse valid window or XBOX values\n"
 # define E_WRONG_EXT "Error :\nFile extension must be .cub\n"
 # define E_EMPTY_FILE "Error :\nEmpty file\n"
-# define E_COLORS_MISS "Error\nColor is missing\n"
-# define E_TEXT_MISS "Error\nTexture is missing\n"
+# define E_COLORS_MISS "Error :\nColors are missing\n"
+# define E_TEXT_MISS "Error :\nTextures are missing\n"
 # define E_WRONG_PATH "Error :\nWrong path for texture\n"
 # define E_BAD_FD "Error :\nOpen failed\n"
-# define E_MISSING_PLAYER "Error:\nMissing player char\n"
+# define E_MISSING_PLAYER "Error :\nMissing player char\n"
 # define E_PARSING_TEXTURES "Error :\nTextures parsing issue\n"
 # define E_PARSING_COLORS "Error :\nColors parsing issue\n"
 # define E_BAD_COLOR_USAGE "Error :\nEmpty field or bad format. Enter 3 values between 0 and 255\n"
@@ -28,5 +28,6 @@
 # define E_BAD_GRID_PARSING "Error :\nMap should be surrounded by walls\n"
 # define E_BAD_GRID_MISSING "Error :\nMap missing\n"
 # define E_BAD_MAP_SIZE "Error :\nMap too long\n"
+# define E_WRONG_PARA "Error :\nWrong parameters\n"
 
 #endif
--- includes/error_bonus.h
+++ includes/error_bonus.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsaby <tsaby@student.42.fr>                +#+  +:+       +#+        */
+/*   By: egache <egache@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 13:18:37 by tsaby             #+#    #+#             */
-/*   Updated: 2025/11/28 19:02:06 by tsaby            ###   ########.fr       */
+/*   Updated: 2025/12/02 14:38:46 by egache           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,11 @@
 # define E_WRONG_INIT "Error :\nUse valid window or XBOX values\n"
 # define E_WRONG_EXT "Error :\nFile extension must be .cub\n"
 # define E_EMPTY_FILE "Error :\nEmpty file\n"
-# define E_COLORS_MISS "Error\nColor is missing\n"
-# define E_TEXT_MISS "Error\nTexture is missing\n"
+# define E_TEXT_MISS "Error\nTextures are missing\n"
 # define E_MISSING_PLAYER "Error:\nMissing player char\n"
 # define E_WRONG_PATH "Error :\nWrong path for texture\n"
 # define E_PARSING_TEXTURES "Error :\nTextures parsing issue\n"
-# define E_PARSING_COLORS "Error :\nColors parsing issue\n"
 # define E_BAD_FD "Error :\nOpen failed\n"
-# define E_MISSING_TEXTURE "Error :\nOne or multiple textures is missing\n"
-# define E_BAD_COLOR_USAGE "Error :\nEmpty field or bad format. Enter 3 values between 0 and 255\n"
 # define E_BAD_CHAR_PARSING "Error :\nWrong grid character : %c\n"
 # define E_BAD_GRID_PARSING "Error :\nMap should be surrounded by walls\n"
 # define E_BAD_GRID_MISSING "Error :\nMap missing\n"
